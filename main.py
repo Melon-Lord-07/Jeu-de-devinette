dc0ff4007c9a786500c9190ea9ef980ca5f1fc7a
Melon-Lord-07 <112639510+Melon-Lord-07@users.noreply.github.com>
2022-09-29 08:39:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-#sandra nitchi
-#TP2
+"""
+Sandra Nitchi
+Groupe 405
+TP2
+
+Ce programme est un jeu devinette
+le joueur choisis un nombre minimale et maximale, et doivent ensuite essayer de deviner le nombre aleatoir choisi entre le max et min
+le programme va indiquer si les essaies sont plus petits ou plus grands que la nombre choissi, jusqu'a que le joueur la devine
+le joueur va ensuite avoir l'option de rejouer, ou quitter
+"""
+
 
 import random
 
 def bornes():
+
+    """
+    Fonction qui sert a definir la borne minimale et maximale du jeu de devinette
+    """
+
     global minimum, maximum
     minimum = int(input("Choissisez un nombre minimale:"))
     maximum = int(input("Choissisez un nombre maximale:"))
     print(f"jai choissi une nombre entre {minimum} et {maximum}! Essayez de la deviner")
 
 def jeu_devinette():
+
+    """
+    Fonction du jeu de devinnette
+    il compte le nombre d'essaies, indique au joueur si le nombre est plus grand ou plus petit que la reponse, et offre l'option de rejouer apres que le joueur a gagné
+    """
 
     bornes()
     number = random.randint(minimum, maximum)
